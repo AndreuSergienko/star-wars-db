1f7b3eef56d036de40f2f32dd75487ff92b0b006
Andrey <andrey.sergienko.dev@gmail.com>
2022-12-30 01:27:37 +0300

    add nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import "./Nav.css";
+
+export class Nav extends Component {
+	render() {
+		return (
+			<nav className="nav">
+				<ul className="nav-list">
+					<li className="nav-item">
+						<a href="#" className="nav-link">
+							People
+						</a>
+					</li>
+					<li className="nav-item">
+						<a href="#" className="nav-link">
+							Planets
+						</a>
+					</li>
+					<li className="nav-item">
+						<a href="#" className="nav-link">
+							Starships
+						</a>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+}
