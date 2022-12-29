ff9373a7a6b58fdab3dde14f6266d973a248fecc
Andrey <andrey.sergienko.dev@gmail.com>
2022-12-30 01:26:43 +0300

    empty

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
-import SwapiService from './services'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components/App";
+import { Swapi } from "./services";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
-
-
-
