3d782000073803b785eece6fda7a2fc24c405f7d
Andrey <andrey.sergienko.dev@gmail.com>
2022-12-30 01:26:37 +0300

    added styles for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+import { Nav } from "../Nav";
+
+export class Header extends Component {
+	render() {
+		return (
+			<header className="header">
+				<div className="container">
+					<div className="header-inner">
+						<h1 className="header-title">Star DB</h1>
+						<Nav />
+					</div>
+				</div>
+			</header>
+		);
+	}
+}
